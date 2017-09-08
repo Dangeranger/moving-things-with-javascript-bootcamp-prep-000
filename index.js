37216cfa926310d1a4637cc57f90686176567ac6
Joshua Burke <burke.joshua.james@gmail.com>
2017-09-08 00:07:00 -0400

    Implemented the movement functions, added listeners

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-const app = "I don't do much."
+document.addEventListener('keydown', function (e) {
+  if (e.which === 37) {
+    moveDodgerLeft()
+    return;
+  } else if (e.which === 39) {
+    moveDodgerRight()
+    return;
+  }
+})
+
+const dodger = document.getElementById('dodger')
+
+function moveDodgerLeft() {
+  var leftEdge = dodger.style.left.replace('px', '')
+  var left = parseInt(leftEdge, 10)
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`
+  }
+}
+
+function moveDodgerRight() {
+  var leftEdge = dodger.style.left.replace('px', '')
+  var left = parseInt(leftEdge, 10)
+  if (left < 360) {
+    dodger.style.left = `${left + 1}px`
+  }
+}
